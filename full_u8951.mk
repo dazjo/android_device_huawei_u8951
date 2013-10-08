7af68cb239a21444ca45924b026f94de68c693db
Daz Jones <yuki@thebronasium.com>
2013-10-08 03:06:07 +0100

    u8951: initial commit

---
+++ full_u8951.mk
@@ -0,0 +1,26 @@
+#
+# Copyright 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit u8833 definitions
+$(call inherit-product, device/huawei/u8833/full_u8833.mk)
+
+# Inherit from u8951 device
+$(call inherit-product, device/huawei/u8951/u8951.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := full_u8951
+PRODUCT_DEVICE := u8951
+PRODUCT_MODEL := Ascend G510
---
+++ u8951.mk
@@ -0,0 +1,29 @@
+#
+# Copyright 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS += device/huawei/u8951/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.confg.hw_appfsversion=U8951V4_3_SYSIMG \
+    ro.confg.hw_appsbootversion=U8951V4_3_APPSBOOT \
+    ro.confg.hw_appversion=U8951V4_3_KERNEL
+
+$(call inherit-product-if-exists, vendor/huawei/u8951/u8951-vendor.mk)

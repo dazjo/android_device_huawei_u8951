e866ca12d839a2bbd5690e286c4b366001a49d86
Daz Jones <yuki@thebronasium.com>
2013-11-06 04:02:51 +0000

    u8951: we kitkat now

--- u8951.mk
+++ u8951.mk
@@ -22,8 +22,8 @@ PRODUCT_COPY_FILES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.confg.hw_appfsversion=U8951V4_3_SYSIMG \
-    ro.confg.hw_appsbootversion=U8951V4_3_APPSBOOT \
-    ro.confg.hw_appversion=U8951V4_3_KERNEL
+    ro.confg.hw_appfsversion=U8951V4_4_SYSIMG \
+    ro.confg.hw_appsbootversion=U8951V4_4_APPSBOOT \
+    ro.confg.hw_appversion=U8951V4_4_KERNEL
 
 $(call inherit-product-if-exists, vendor/huawei/u8951/u8951-vendor.mk)
